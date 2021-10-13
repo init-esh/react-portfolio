9d30121f98a10dd5e3248d5c8c4e68752e8b1cbb
Nitesh Soni <24539834+init-esh@users.noreply.github.com>
2021-10-13 20:21:16 +0530

    add intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,41 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      strings: ["Developer", "Designer", "Content Creator"]
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      <a href="#contact">Contact</a>
+      <div className="left">
+        <div className="imgContainer">
+          <img
+            src="https://raw.githubusercontent.com/safak/youtube/react-portfolio/public/assets/man.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There I'm</h2>
+          <h1>Nitesh Soni</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img
+            src="https://raw.githubusercontent.com/safak/youtube/react-portfolio/public/assets/down.png"
+            alt=""
+          />
+        </a>
+      </div>
     </div>
   );
 }
