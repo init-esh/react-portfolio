dadcc28a24d09f00b176d7e23f5952b533fb1bdf
Nitesh Soni <24539834+init-esh@users.noreply.github.com>
2021-11-09 10:42:53 +0530

    Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return <div className="portfolio" id="portfolio">
+    <h1>Portfolio</h1>
+  </div>;
 }
